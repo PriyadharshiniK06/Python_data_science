d5c1cefb65fb0471b0a89364cd3f65c0c302fbbd
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-09-30 11:55:12 +0530

    Add files via upload

---
+++ type of the following.py
@@ -0,0 +1,7 @@
+#Find the type of the following using the type command:
+print(type("Hello World"))
+#Find the type of the following using the type command:
+print(type("1.1"))
+#Cast the following to an int and then to a bool :
+#"1"
+print(type(bool(int("1"))))
