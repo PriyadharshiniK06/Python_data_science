72fb7568bf831b532aa894e5a469d2c4927de1f7
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-09-30 22:04:47 +0530

    Add files via upload

---
+++ your first program lab.py
@@ -0,0 +1,191 @@
+# Try your first Python output
+
+print('Hello, Python!')
+
+# Check the Python Version
+
+import sys
+print(sys.version)
+
+# Practice on writing comments
+
+print('Hello, Python!') # This line prints a string
+# print('Hi')
+
+# Print string as error message
+
+#frint("Hello, Python!")->error
+print("Hello, Python!")
+
+# Try to see built-in error message
+
+print("Hello, Python!")
+      
+# Print string and error to see the running order
+
+print("This will be printed")
+#frint("This will cause an error")
+
+print("This will NOT be printed")
+
+#Exercise
+
+#In the code cell below, use the print() function to print out the phrase: Hello, world!
+
+print ("Hello world!")
+
+#Now, let's enhance your code with a comment. In the code cell below, print out the phrase: Hello, world! and comment it with the phrase Print the traditional hello world all in one line of code
+      
+print("Hello, world!") # Print the traditional hello world
+
+#Example of objects
+
+# Integer
+
+print(11)
+
+# Float
+
+print(2.14)
+
+#String
+
+print("Hello, Python 101!")
+
+#to tell the type of the objects
+
+
+print(type(12))
+print(type(2.14))
+print(type("Hello, Python 101!"))
+print(type(12.0))
+
+#int
+
+print(type(-1))
+print(type(4))
+print(type(0))
+
+#float
+
+print(type(0.5))
+print(type(0.56))
+print(type(1.0))
+
+# System settings about float type
+
+print(sys.float_info)
+
+#Typecasting
+
+print(type(2))
+print(float(2))
+print(type(float(2)))
+
+print(int(1.1))
+print(int('1'))
+#int('1 or 2 people') this will create an error because string cannot be converted into int
+print(float('1.2'))
+print(str(1))
+print(str(1.2))
+
+#boolean
+
+print(type(True))
+print(type(False))
+print(int(True))
+print(bool(1))
+print(bool(0))
+print(float(True))
+
+#Typecasting
+
+print(type(6/2))
+print(type(6//2))
+
+# Addition operation expression
+
+print(43 + 60 + 16 + 41)
+
+# Subtraction operation expression
+
+print(50 - 60)
+
+# Multiplication operation expression
+
+print(5 * 5)
+
+# Division operation expression
+
+print(25 / 5)
+
+# Division operation expression
+
+print(25 / 6)
+
+# Integer division operation expression
+
+print(25 // 5)
+
+# Integer division operation expression
+
+print(25 // 6)
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+
+print(160//60)
+print(160/60)
+
+# Mathematical expression
+
+print(30 + 2 * 60)
+
+# Mathematical expression
+
+print((30 + 2) * 60)
+
+# Store value into variable
+x = 43 + 60 + 16 + 41
+
+# Print out the value in variable
+
+print(x)
+
+# Use another variable to store the result of the operation between variable and value
+
+y = x / 60
+print(y)
+
+
+# Overwrite variable with new value
+
+x = x / 60
+print(x)
+
+# Name the variables meaningfully
+
+total_min = 43 + 42 + 57 # Total length of albums in minutes
+print(total_min)
+
+# Name the variables meaningfully
+
+total_hours = total_min / 60 # Total length of albums in hours 
+print(total_hours)
+
+# Complicate expression
+
+total_hours = (43 + 42 + 57) / 60  # Total hours in a single expression
+print(total_hours)
+
+#Exercise: Expression and Variables in Python
+
+x = 3 + 2 * 2
+y=(3+2)*2
+z=x+y
+print(z)
+
+
+
+
+      
+
