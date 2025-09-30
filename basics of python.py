0c95745ebb406b38bc8828ecb8d2d0ce45def5cc
PriyadharshiniK06 <priyadharshini.kumaran06@gmail.com>
2025-09-30 11:51:06 +0530

    Add files via upload

---
+++ basics of python.py
@@ -0,0 +1,6 @@
+#Let's say hello to the world with a PRINT statement.
+print("Hello World")
+#Let's print "Hello" and "World" on two separate lines.
+print("Hello\nWorld")
+#In Python, comments are created by putting a hash mark # in front of the text of the comment.
+# this is a comment
